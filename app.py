b8dd0a845f11ee04e39d426af5d79ead4079b3f8
GRuvira18 <guillemoruvira99@gmail.com>
2019-11-01 18:06:46 +0100

    Act

--- app.py
+++ app.py
@@ -5,8 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
-
+    return render_template('index.html') #prueba123
 @app.route('/params')
 @app.route('/params/<name>')
 def params(name= 'default'):
